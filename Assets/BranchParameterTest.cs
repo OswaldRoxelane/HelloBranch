8b2a392ca460d5301820aaeb84dece22647dea9c
Oswald de Bruin <oswald@roxelane.nl>
2018-09-27 16:33:36 +0200

    Crude working version up

---
+++ Assets/BranchParameterTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BranchParameterTest : MonoBehaviour {
+    public Text dataDump;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        if (dataDump != null)
+        {
+            dataDump.text = "Hello Branch";
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Prototyping/BranchUnityTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BranchUnityTest : MonoBehaviour {
+    public Text dataDump;
+    public Text urlDump;
+
+    void Start()
+    {
+        Branch.initSession(CallbackWithBranchUniversalObject);
+    }
+
+    void CallbackWithBranchUniversalObject(BranchUniversalObject buo,
+                                            BranchLinkProperties linkProps,
+                                            string error)
+    {
+        if (error != null)
+        {
+            System.Console.WriteLine("Error : "
+                                    + error);
+        }
+        else if (linkProps.controlParams.Count > 0)
+        {
+            System.Console.WriteLine("Deeplink params : "
+                                    + buo.ToJsonString()
+                                    + linkProps.ToJsonString());
+            dataDump.text = "Deeplink params : "
+                                    + buo.ToJsonString()
+                                    + linkProps.ToJsonString();
+        }
+    }
+
+    private void Awake()
+    {
+        BranchLinkProperties linkProperties = new BranchLinkProperties();
+        linkProperties.alias = "UnityAndroidAddTest" + System.DateTime.Now;
+        linkProperties.tags.Add("VlaFlip" + System.DateTime.Now);
+        linkProperties.controlParams.Add("PostMoment", System.DateTime.Now.ToString());
+        // Feature link is associated with. Eg. Sharing
+        linkProperties.feature = "Boodschappen";
+        // The channel where you plan on sharing the link Eg.Facebook, Twitter, SMS etc
+        linkProperties.channel = "Twitter";
+        linkProperties.stage = "1";
+        // Parameters used to control Link behavior
+        linkProperties.controlParams.Add("$desktop_url", "http://roxelane.nl/portfolio-items/a2-racer/?portfolioCats=162");
+
+        var universalObject = new BranchUniversalObject();
+        
+        
+
+        Branch.getShortURL(universalObject, linkProperties, (p, error) => {
+            if (error != null)
+            {
+                Debug.LogError("Branch.getShortURL failed: " + error);
+                urlDump.text = "Branch.getShortURL failed: " + error;
+            }
+            else if (p != null) {
+                Debug.Log("Branch.getShortURL shared params: " + p);
+                urlDump.text = "Branch.getShortURL shared params: " + p;
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
